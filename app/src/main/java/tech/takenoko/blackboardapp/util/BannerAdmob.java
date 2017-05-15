9b2da73bc69e6dcccc06922f01dff10301b7a07a
Takenoko <michaeelbobu@gmail.com>
2017-05-16 01:57:24 +0900

    fix admob

--- app/src/main/java/tech/takenoko/blackboardapp/util/BannerAdmob.java
+++ app/src/main/java/tech/takenoko/blackboardapp/util/BannerAdmob.java
@@ -1,7 +1,9 @@
 package tech.takenoko.blackboardapp.util;
 
 import android.content.Context;
+import android.util.Log;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
@@ -15,6 +17,8 @@ import tech.takenoko.blackboardapp.R;
 
 public class BannerAdmob {
 
+    final static String log = "----BannerAdmob----";
+
     static AdView mAdView;
 
     public static void setup(final Context context) {
@@ -22,5 +26,28 @@ public class BannerAdmob {
         mAdView = (AdView) ((MainActivity)context).findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdFailedToLoad(int i) {
+                switch (i) {
+                    case AdRequest.ERROR_CODE_INTERNAL_ERROR:
+                        Log.i(log, "ERROR_CODE_INTERNAL_ERROR");
+                        break;
+                    case AdRequest.ERROR_CODE_INVALID_REQUEST:
+                        Log.i(log, "ERROR_CODE_INVALID_REQUEST");
+                        break;
+                    case AdRequest.ERROR_CODE_NETWORK_ERROR:
+                        Log.i(log, "ERROR_CODE_NETWORK_ERROR");
+                        break;
+                    case AdRequest.ERROR_CODE_NO_FILL:
+                        Log.i(log, "ERROR_CODE_NO_FILL");
+                        break;
+                }
+            }
+            @Override
+            public void onAdLoaded() {
+                Log.i(log, "onAdLoaded");
+            }
+        });
     }
 }
